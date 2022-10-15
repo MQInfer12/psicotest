33bcc416523d950d3c2267b65faa7a26e70865bc
JosseMontano <josemariazambranamontano123@gmail.com>
2022-10-15 17:46:22 -0400

    solucionando problemas de scroll en el chat

--- client/src/components/chat/messages.jsx
+++ client/src/components/chat/messages.jsx
@@ -3,15 +3,10 @@ import styled from "styled-components";
 import Message from "./message";
 
 const Container = styled.div`
-  flex: 1;
-  border-right: 1px solid #3d3c61;
-  background-color: #3e3c61;
-  .messages {
-    background-color: #ddddf7;
-    padding: 10px;
-    height: calc(100% - 160px);
-    overflow: scroll;
-  }
+  background-color: #ddddf7;
+  padding: 10px;
+  height: calc(100% - 160px);
+  overflow: scroll;
 `;
 
 const Messages = () => {
