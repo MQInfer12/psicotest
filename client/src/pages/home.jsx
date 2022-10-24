60f43bdf0ff4bae67a44562f0a276ca8b31756e8
MQInfer12 <maummq@gmail.com>
2022-10-23 23:57:47 -0400

    Toque personal

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import { useEffect } from 'react';
+import styled from 'styled-components';
+import Secret from '../images/home.mp3';
+
+const DivEntero = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  align-items: center;
+  justify-content: center;
+  color: #ADA7A7;
+  font-size: 16px;
+  font-weight: 300;
+`;
 
 const Home = () => {
+  useEffect(() => {
+    const sound = new Audio(Secret);
+    sound.play();
+  }, []);
+
   return (
-    <>
-      <div>Bienvenido</div>
-    </>
+    <DivEntero>
+      <p>¡Es peligroso ir solo, toma esto!</p>
+      <i className="fa-solid fa-mug-hot"></i>
+      <p>Mauricio Molina</p>
+    </DivEntero>
   )
 }
 
