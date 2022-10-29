227947a05d68043dd246cb2e4941dcc94c9c4948
JosseMontano <josemariazambranamontano123@gmail.com>
2022-10-29 11:32:26 -0400

    Reporte Pdf completo de las respuestas

--- server/routes/api.php
+++ server/routes/api.php
@@ -78,6 +78,7 @@ Route::group(['middleware' => 'api'], function(){
 
     //RESPUESTA ROUTES
     Route::apiResource("respuesta", RespuestaController::class);
+    Route::get("respuesta/pdf/{id}", [RespuestaController::class, 'indexPdf']);
     Route::get("respuesta/my/{email}", [RespuestaController::class, 'myProffessors']);
     Route::get("respuesta/test/{id}", [RespuestaController::class, 'getIdTest']);
     Route::get("respuesta/docente/{id}", [RespuestaController::class, 'getRespuestasByDocente']);
