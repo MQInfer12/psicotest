0e8c7e8f52051da87e677b46ca01804499a662fd
MQInfer12 <maummq@gmail.com>
2022-11-04 14:52:55 -0400

    New developers page

--- client/src/pages/developers.jsx
+++ client/src/pages/developers.jsx
@@ -1,100 +1,118 @@
-import React from "react";
+import React from 'react'
+import styled from 'styled-components';
+import Navbar from '../components/landing/navbar'
+import { useWindowHeight } from '../hooks/useWindowHeight';
 import MauriPhoto from "../assets/developers/mauri.jpg";
 import JosePhoto from "../assets/developers/joseZambrana.jpg";
-import styled from "styled-components";
-import Navbar from "../components/landing/navbar";
 
-const Container = styled.div`
-  display: grid;
-  place-content: center;
-  height: 100vh;
-`;
-const Title = styled.h2`
-  font-weight: bold;
-  text-transform: uppercase;
-  font-size: 32px;
-  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
-  text-align: center;
-  padding: 10px;
-`;
-const Cardontaner = styled.div`
+const AllContainer = styled.div`
+  height: ${props => props.height};
   display: flex;
   justify-content: center;
-  @media (max-width: 630px) {
-    flex-direction: column;
-  }
+  overflow: hidden;
 `;
-const Card = styled.div`
-  width: auto;
-  text-align: center;
-  margin-right: 30px;
-  margin-top: 30px;
-  img {
-    width: 200px;
-    height: 200px;
-    border-radius: 100%;
-  }
+
+const ContainerPage = styled.div`
+  min-width: 105%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
-const Name = styled.h3`
-  color: #000;
-  font-weight: bold;
-  font-size: 22px;
-`;
-const Des = styled.p`
-  color: #000;
-  font-size: 18px;
-  margin-bottom: 10px;
-`;
-const OpenAccountButton = styled.a`
-  border: none;
-  width: fit-content;
-  background: linear-gradient(180deg, #6209db 0%, #7613fd 100%);
-  border-radius: 6px;
-  padding: 10px;
-  color: #ffffff;
-  font-weight: 300;
-  transition: all 0.3s;
-  text-decoration: none;
-  cursor: pointer;
+
+const Column = styled.div`
+  width: 100%;
+  height: 100%;
+  transition: all 0.5s;
+  transform: skewX(-5deg);
+  background-color: #f5f5f5;
+  display: grid;
+  place-content: center;
+
   &:hover {
-    filter: grayscale(0.5);
+    width: calc(100% + 30px);
+    background-color: #6209db;
+
+    & .nombres {
+      color: #FFFFFF;
+    }
+  }
+
+  & > div {
+    transform: skewX(5deg);
   }
 `;
+
+const DivInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  align-items: center;
+  justify-content: center; 
+`;
+
+const NombreDevs = styled.h2`
+  font-size: 40px;
+  font-weight: 700;
+  transition: all 0.5s;
+`;
+
+const PhotoDevs = styled.img`
+  width: 150px;
+  height: 150px;
+  object-fit: cover;
+  border-radius: 30px;
+`;
+
+const DescripcionDevs = styled.h3`
+  font-size: 16px;
+  font-weight: 500;
+  transition: all 0.5s;
+`;
+
+const ButtonContact = styled.button`
+  padding: 10px 20px;
+  background-color: #FFFFFF;
+  color: #6209db;
+  font-weight: 500;
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+  transition: all 0.2s;
+
+  &:hover {
+    opacity: 0.8;
+  }
+`;
+
 const Developers = () => {
-  const apiWhatsap = "https://api.whatsapp.com/send?phone=";
+  const windowHeight = useWindowHeight();
+
   return (
     <>
       <Navbar />
-      <Container>
-        <Title>El proyecto no seria posible si no fuera gracias a:</Title>
-        <Cardontaner>
-          <Card>
-            <img src={MauriPhoto} alt="" />
-            <Name>Mauricio Molina</Name>
-            <Des>Desarrollador Fullstack</Des>
-            <OpenAccountButton
-              href={apiWhatsap + "59176407344"}
-              target={"_blank"}
-            >
-              Contactar
-            </OpenAccountButton>
-          </Card>
-
-          <Card>
-            <img src={JosePhoto} alt="" />
-            <Name>José Zambrana</Name>
-            <Des>Desarrollador Fullstack</Des>
-            <OpenAccountButton
-              href={apiWhatsap + "59165722183"}
-              target={"_blank"}
-            >
-              Contactar
-            </OpenAccountButton>
-          </Card>
-        </Cardontaner>
-      </Container>
+      <AllContainer height={windowHeight}>
+        <ContainerPage>
+          <Column>
+            <DivInfo>
+              <NombreDevs className='nombres'>Mauricio Molina</NombreDevs>
+              <DescripcionDevs className='nombres'>Full Stack Developer</DescripcionDevs>
+              <PhotoDevs src={MauriPhoto} />
+              <ButtonContact>Contactar</ButtonContact>
+            </DivInfo>
+          </Column>
+          <Column>
+            <DivInfo>
+              <NombreDevs className='nombres'>Jose Zambrana</NombreDevs>
+              <DescripcionDevs className='nombres'>Full Stack Developer</DescripcionDevs>
+              <PhotoDevs src={JosePhoto} />
+              <ButtonContact>Contactar</ButtonContact>
+            </DivInfo>
+          </Column>
+        </ContainerPage>
+      </AllContainer>
     </>
-  );
-};
+  )
+}
 
-export default Developers;
+export default Developers
